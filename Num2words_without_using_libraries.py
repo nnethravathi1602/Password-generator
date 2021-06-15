a74f8cad5d3f9c99613f06b3914bd4350afe905f
Nethravathi M N <nnethravathi1602@gmail.com>
2021-06-15 13:41:26 +0530

    Update Num2words_without_using_libraries.py

--- Num2words_without_using_libraries.py
+++ Num2words_without_using_libraries.py
@@ -10,24 +10,27 @@ num_2 = {2:'Twenty', 3:'Thirty', 4:'Forty', 5:'Fifty',6: 'Sixty', 7:'Seventy',8:
 print('### Welcome to number to word converter ###')
 
 while True:
- print('~'*45)
- number=input('Enter a number / quit : ')
- print('~'*45)
- if number=='quit':
-  break
- try:
-  def word(num):
-   if (0<= int(num) <= 19):
-    print('The number',num,'in words =',num_1[num])
-   elif (20<= int(num) <= 100):
-    S1=(num//10)
-    S2=(num%10)
-    if S2!=0:
-     print('The number',num,'in words =',(num_2[S1]+'-'+num_1[S2]))
-    else:
-     print('The number',num,'in words =',num_2[S1])
-   else:
-    print('Enter the number between the 0 to 100 range')
-  word(int(number))
- except:
-  print('Please enter the number')
+   print('~'*45)
+   number=input('Enter a number / quit : ')
+   print('~'*45)
+
+   if number=='quit':
+      break
+   try:
+      def word(num):
+         if (0<= int(num) <= 19):
+            print('The number',num,'in words =',num_1[num])
+
+         elif (20<= int(num) <= 100):
+            S1=(num//10)
+            S2=(num%10)
+
+            if S2!=0:
+               print('The number',num,'in words =',(num_2[S1]+'-'+num_1[S2]))
+            else:
+               print('The number',num,'in words =',num_2[S1])
+         else:
+            print('Enter the number between the 0 to 100 range')
+            word(int(number))
+   except:
+      print('Please enter the number')
